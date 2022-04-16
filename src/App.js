ee144e0b1a0f1ad474babb6b5673aeebb00aebdb
sofiree <muosophia0@gmail.com>
2022-04-16 17:40:18 +0100

    set up router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Ratings } from './components';
 
 
 function App() {
   return (
     <React.Fragment>
-
+      <Routes>
+        <Route path='/' element={<Ratings/>}/>
+      </Routes>
     </React.Fragment>
   );
 }
